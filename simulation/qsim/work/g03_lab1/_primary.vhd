fd2de3687655edec598d6e1bf0588e4eb2bf61d9
alexH2456 <alexander.harris@mail.mcgill.ca>
2017-10-10 17:25:07 -0400

    Finished Assignment 1

--- simulation/qsim/work/g03_lab1/_primary.vhd
+++ simulation/qsim/work/g03_lab1/_primary.vhd
@@ -2,8 +2,8 @@ library verilog;
 use verilog.vl_types.all;
 entity g03_lab1 is
     port(
-        floor           : out    vl_logic_vector(2 downto 0);
+        Amod13          : out    vl_logic_vector(3 downto 0);
         A               : in     vl_logic_vector(5 downto 0);
-        S               : out    vl_logic_vector(3 downto 0)
+        floor           : out    vl_logic_vector(2 downto 0)
     );
 end g03_lab1;
