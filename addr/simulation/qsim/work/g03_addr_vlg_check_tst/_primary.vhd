8eba46d2203aab751446637a8fce9e86b12d0d0c
alexH2456 <alexander.harris@mail.mcgill.ca>
2017-10-10 17:36:21 -0400

    Reorganized file structure

--- addr/simulation/qsim/work/g03_addr_vlg_check_tst/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity g03_addr_vlg_check_tst is
-    port(
-        C0              : in     vl_logic;
-        S               : in     vl_logic_vector(5 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end g03_addr_vlg_check_tst;
--- addr/simulation/qsim/work/g03_addr_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity g03_addr_vlg_vec_tst is
-end g03_addr_vlg_vec_tst;
