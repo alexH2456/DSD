e7e8568d2cacf460dc02f7cbb3e63cfd366cc8ae
alexH2456 <alexander.harris@mail.mcgill.ca>
2017-11-09 19:36:16 -0500

    Lab 3 - Completed Report

--- Lab3/simulation/qsim/work/g03_lab3/_primary.vhd
+++ Lab3/simulation/qsim/work/g03_lab3/_primary.vhd
@@ -2,14 +2,21 @@ library verilog;
 use verilog.vl_types.all;
 entity g03_lab3 is
     port(
+        altera_reserved_tms: in     vl_logic;
+        altera_reserved_tck: in     vl_logic;
+        altera_reserved_tdi: in     vl_logic;
+        altera_reserved_tdo: out    vl_logic;
         empty           : out    vl_logic;
-        enable          : in     vl_logic;
+        clock           : in     vl_logic;
         reset           : in     vl_logic;
-        clk             : in     vl_logic;
-        addr            : in     vl_logic_vector(5 downto 0);
-        data            : in     vl_logic_vector(5 downto 0);
+        enable          : in     vl_logic;
+        mode_floor      : in     vl_logic;
+        mode_mod        : in     vl_logic;
+        address         : in     vl_logic_vector(5 downto 0);
         mode            : in     vl_logic_vector(1 downto 0);
         full            : out    vl_logic;
+        \7_segment_floor\: out    vl_logic_vector(6 downto 0);
+        \7_segment_mod\ : out    vl_logic_vector(6 downto 0);
         num             : out    vl_logic_vector(5 downto 0);
         value           : out    vl_logic_vector(5 downto 0)
     );
--- Lab3/simulation/qsim/work/g03_lab3_vlg_sample_tst/_primary.vhd
+++ Lab3/simulation/qsim/work/g03_lab3_vlg_sample_tst/_primary.vhd
@@ -2,11 +2,12 @@ library verilog;
 use verilog.vl_types.all;
 entity g03_lab3_vlg_sample_tst is
     port(
-        addr            : in     vl_logic_vector(5 downto 0);
-        clk             : in     vl_logic;
-        data            : in     vl_logic_vector(5 downto 0);
+        address         : in     vl_logic_vector(5 downto 0);
+        clock           : in     vl_logic;
         enable          : in     vl_logic;
         mode            : in     vl_logic_vector(1 downto 0);
+        mode_floor      : in     vl_logic;
+        mode_mod        : in     vl_logic;
         reset           : in     vl_logic;
         sampler_tx      : out    vl_logic
     );
